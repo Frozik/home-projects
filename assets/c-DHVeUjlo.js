8ded15b55436bb003158c0990c95907ca82ee190
Dmitry Sharov <frozik@gmail.com>
2024-09-09 02:29:58 +0300

    Updates

---
+++ assets/c-DHVeUjlo.js
@@ -0,0 +1,1 @@
+import{bV as i}from"./e-CS5-0qm2.js";function c(e,t){var r=-1,n=e.length;for(t||(t=Array(n));++r<n;)t[r]=e[r];return t}var p=function(){try{var e=i(Object,"defineProperty");return e({},"",{}),e}catch{}}();export{c,p as d};
